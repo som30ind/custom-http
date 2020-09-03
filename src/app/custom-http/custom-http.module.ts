c84b1116ee5b970bad812741f4591d9084be3274
som30ind <som30ind@gmail.com>
2020-09-03 19:31:18 +0530

    Initial Version HTTP Module

--- src/app/custom-http/custom-http.module.ts
+++ src/app/custom-http/custom-http.module.ts
@@ -2,7 +2,6 @@ import { NgModule, Injectable } from "@angular/core";
 import {
   Http,
   RequestOptions,
-  BrowserXhr,
   BaseRequestOptions,
   ResponseOptions,
   BaseResponseOptions,
@@ -18,11 +17,101 @@ import {
   ResponseType,
   ResponseContentType
 } from "@angular/http";
-import { Observable, Observer } from "rxjs";
+import { Observable, Observer, fromEvent, throwError } from "rxjs";
+import { map, flatMap, catchError } from "rxjs/operators";
 // import { isSuccess } from "@angular/http/src/http_utils";
 // import { ContentType } from "@angular/http/src/enums";
 
+interface KeyValuePair<T> {
+  [key: string]: T;
+}
+
+interface MobileHttpClientResponse {
+  status: number;
+  url: string;
+  headers: KeyValuePair<string>;
+}
+
+interface MobileHttpClientSuccessResponse extends MobileHttpClientResponse {
+  data: string;
+}
+
+interface MobileHttpClientErrorResponse extends MobileHttpClientResponse {
+  error: string;
+}
+
+class MobileHttpClientOptions {
+  method: string = "GET";
+  headers: KeyValuePair<string> = {};
+  responseType: string = "json";
+  data: any;
+}
+
+interface HttpPlugin {
+  sendRequest(
+    url: string,
+    options: MobileHttpClientOptions,
+    sCb: (r: MobileHttpClientSuccessResponse) => void,
+    eCb: (r: MobileHttpClientErrorResponse) => void
+  ): void;
+}
+
+interface CordovaHttpPlugin extends HttpPlugin {}
+
+interface CordovaPlugin {
+  http: CordovaHttpPlugin;
+}
+
+interface Cordova {
+  plugin: CordovaPlugin;
+}
+
+declare const cordova: Cordova;
 const XSSI_PREFIX = /^\)\]\}',?\n/;
+
+@Injectable()
+export class MobileHttpClient {
+  sendRequest(
+    url: string,
+    options: MobileHttpClientOptions
+  ): Observable<MobileHttpClientSuccessResponse> {
+    return fromEvent(window, "deviceReady").pipe(
+      flatMap(() => {
+        return this.sendCordovaRequestAsObservable(url, options);
+      })
+    );
+  }
+
+  private sendCordovaRequestAsObservable(
+    url: string,
+    options: MobileHttpClientOptions
+  ): Observable<MobileHttpClientSuccessResponse> {
+    return Observable.create(
+      (obs: Observer<MobileHttpClientSuccessResponse>) => {
+        cordova.plugin.http.sendRequest(
+          url,
+          options,
+          response => {
+            obs.next(response);
+            obs.complete();
+          },
+          response => {
+            obs.error(response);
+          }
+        );
+      }
+    );
+  }
+
+  private sendHttpRequestAsObservable(
+    url: string,
+    options: MobileHttpClientOptions
+  ): Observable<any> {
+    return Observable.create((obs: Observer<any>) => {
+      obs.next({});
+    });
+  }
+}
 
 export enum ContentType {
   NONE,
@@ -47,148 +136,132 @@ export function getResponseURL(xhr: any): string | null {
   return null;
 }
 
-export class CordovaHttpConnection implements Connection {
+export class MobileHttpConnection implements Connection {
   request: Request;
-  /**
-   * Response {@link EventEmitter} which emits a single {@link Response} value on load event of
-   * `XMLHttpRequest`.
-   */
   response: Observable<Response>;
   // TODO(issue/24571): remove '!'.
   readyState!: ReadyState;
+
   constructor(
     req: Request,
-    browserXHR: BrowserXhr,
+    mobileHttpClient: MobileHttpClient,
     baseResponseOptions?: ResponseOptions
   ) {
     this.request = req;
-    this.response = new Observable<Response>(
-      (responseObserver: Observer<Response>) => {
-        const _xhr: XMLHttpRequest = browserXHR.build();
-        _xhr.open(RequestMethod[req.method].toUpperCase(), req.url);
-        if (req.withCredentials != null) {
-          _xhr.withCredentials = req.withCredentials;
-        }
-        // load event handler
-        const onLoad = () => {
-          // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
-          let status: number = _xhr.status === 1223 ? 204 : _xhr.status;
 
-          let body: any = null;
+    const options = new MobileHttpClientOptions();
+    const url = req.url;
+    options.method = RequestMethod[req.method].toUpperCase();
 
-          // HTTP 204 means no content
-          if (status !== 204) {
-            // responseText is the old-school way of retrieving response (supported by IE8 & 9)
-            // response/responseType properties were introduced in ResourceLoader Level2 spec
-            // (supported by IE10)
-            body =
-              typeof _xhr.response === "undefined"
-                ? _xhr.responseText
-                : _xhr.response;
+    // if (req.withCredentials != null) {
+    //   _xhr.withCredentials = req.withCredentials;
+    // }
 
-            // Implicitly strip a potential XSSI prefix.
-            if (typeof body === "string") {
-              body = body.replace(XSSI_PREFIX, "");
-            }
-          }
+    this.setDetectedContentType(req, options.headers);
 
-          // fix status code when it is 0 (0 status is undocumented).
-          // Occurs when accessing file resources or on Android 4.1 stock browser
-          // while retrieving files from application cache.
-          if (status === 0) {
-            status = body ? 200 : 0;
-          }
+    if (req.headers == null) {
+      req.headers = new Headers();
+    }
+    if (!req.headers.has("Accept")) {
+      req.headers.append("Accept", "application/json, text/plain, */*");
+    }
+    req.headers.forEach(
+      (values, name) => (options.headers[name!] = values.join(","))
+    );
 
-          const headers: Headers = Headers.fromResponseHeaderString(
-            _xhr.getAllResponseHeaders()
-          );
-          // IE 9 does not provide the way to get URL of response
-          const url = getResponseURL(_xhr) || req.url;
-          const statusText: string = _xhr.statusText || "OK";
+    // Select the correct buffer type to store the response
+    if (req.responseType != null && options.responseType != null) {
+      switch (req.responseType) {
+        case ResponseContentType.ArrayBuffer:
+          options.responseType = "arraybuffer";
+          break;
 
-          let responseOptions = new ResponseOptions({
-            body,
-            status,
-            headers,
-            statusText,
-            url
-          });
-          if (baseResponseOptions != null) {
-            responseOptions = baseResponseOptions.merge(responseOptions);
-          }
-          const response = new Response(responseOptions);
-          response.ok = isSuccess(status);
-          if (response.ok) {
-            responseObserver.next(response);
-            // TODO(gdi2290): defer complete if array buffer until done
-            responseObserver.complete();
-            return;
-          }
-          responseObserver.error(response);
-        };
-        // error event handler
-        const onError = (err: ErrorEvent) => {
-          let responseOptions = new ResponseOptions({
-            body: err,
-            type: ResponseType.Error,
-            status: _xhr.status,
-            statusText: _xhr.statusText
-          });
-          if (baseResponseOptions != null) {
-            responseOptions = baseResponseOptions.merge(responseOptions);
-          }
-          responseObserver.error(new Response(responseOptions));
-        };
+        case ResponseContentType.Json:
+          options.responseType = "json";
+          break;
 
-        this.setDetectedContentType(req, _xhr);
+        case ResponseContentType.Text:
+          options.responseType = "text";
+          break;
 
-        if (req.headers == null) {
-          req.headers = new Headers();
-        }
-        if (!req.headers.has("Accept")) {
-          req.headers.append("Accept", "application/json, text/plain, */*");
-        }
-        req.headers.forEach((values, name) =>
-          _xhr.setRequestHeader(name!, values.join(","))
-        );
+        case ResponseContentType.Blob:
+          options.responseType = "blob";
+          break;
 
-        // Select the correct buffer type to store the response
-        if (req.responseType != null && _xhr.responseType != null) {
-          switch (req.responseType) {
-            case ResponseContentType.ArrayBuffer:
-              _xhr.responseType = "arraybuffer";
-              break;
-            case ResponseContentType.Json:
-              _xhr.responseType = "json";
-              break;
-            case ResponseContentType.Text:
-              _xhr.responseType = "text";
-              break;
-            case ResponseContentType.Blob:
-              _xhr.responseType = "blob";
-              break;
-            default:
-              throw new Error("The selected responseType is not supported");
-          }
-        }
-
-        _xhr.addEventListener("load", onLoad);
-        _xhr.addEventListener("error", onError);
-
-        _xhr.send(this.request.getBody());
-
-        return () => {
-          _xhr.removeEventListener("load", onLoad);
-          _xhr.removeEventListener("error", onError);
-          _xhr.abort();
-        };
+        default:
+          throw new Error("The selected responseType is not supported");
       }
+    }
+
+    options.data = this.request.getBody();
+    this.response = mobileHttpClient.sendRequest(url, options).pipe(
+      catchError((resp: MobileHttpClientErrorResponse) => {
+        let responseOptions = new ResponseOptions({
+          body: resp.error,
+          type: ResponseType.Error,
+          status: resp.status,
+          statusText: "Error"
+        });
+        if (baseResponseOptions != null) {
+          responseOptions = baseResponseOptions.merge(responseOptions);
+        }
+        return throwError(new Response(responseOptions));
+      }),
+      map(resp => {
+        // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
+        let status: number = resp.status === 1223 ? 204 : resp.status;
+
+        let body: any = null;
+
+        // HTTP 204 means no content
+        if (status !== 204) {
+          // responseText is the old-school way of retrieving response (supported by IE8 & 9)
+          // response/responseType properties were introduced in ResourceLoader Level2 spec
+          // (supported by IE10)
+          body = resp.data;
+
+          // Implicitly strip a potential XSSI prefix.
+          if (typeof body === "string") {
+            body = body.replace(XSSI_PREFIX, "");
+          }
+        }
+
+        // fix status code when it is 0 (0 status is undocumented).
+        // Occurs when accessing file resources or on Android 4.1 stock browser
+        // while retrieving files from application cache.
+        if (status === 0) {
+          status = body ? 200 : 0;
+        }
+
+        const headers: Headers = new Headers(resp.headers);
+        // IE 9 does not provide the way to get URL of response
+        const url = resp.url || req.url;
+        const statusText: string = "OK";
+
+        let responseOptions = new ResponseOptions({
+          body,
+          status,
+          headers,
+          statusText,
+          url
+        });
+        if (baseResponseOptions != null) {
+          responseOptions = baseResponseOptions.merge(responseOptions);
+        }
+        const response = new Response(responseOptions);
+        response.ok = isSuccess(status);
+        if (response.ok) {
+          return response;
+        }
+
+        throw response;
+      })
     );
   }
 
   setDetectedContentType(
     req: any /** TODO Request */,
-    _xhr: any /** XMLHttpRequest */
+    headers: any /** XMLHttpRequest */
   ) {
     // Skip if a custom Content-Type header is provided
     if (req.headers != null && req.headers.get("Content-Type") != null) {
@@ -199,22 +272,24 @@ export class CordovaHttpConnection implements Connection {
     switch (req.contentType) {
       case ContentType.NONE:
         break;
+
       case ContentType.JSON:
-        _xhr.setRequestHeader("content-type", "application/json");
+        headers["content-type"] = "application/json";
         break;
+
       case ContentType.FORM:
-        _xhr.setRequestHeader(
-          "content-type",
-          "application/x-www-form-urlencoded;charset=UTF-8"
-        );
+        headers["content-type"] =
+          "application/x-www-form-urlencoded;charset=UTF-8";
         break;
+
       case ContentType.TEXT:
-        _xhr.setRequestHeader("content-type", "text/plain");
+        headers["content-type"] = "text/plain";
         break;
+
       case ContentType.BLOB:
         const blob = req.blob();
         if (blob.type) {
-          _xhr.setRequestHeader("content-type", blob.type);
+          headers["content-type"] = blob.type;
         }
         break;
     }
@@ -222,18 +297,18 @@ export class CordovaHttpConnection implements Connection {
 }
 
 @Injectable()
-export class CordovaHttpBackend implements ConnectionBackend {
+export class MobileHttpBackend implements ConnectionBackend {
   constructor(
-    private _browserXHR: BrowserXhr,
+    private mobileHttpClient: MobileHttpClient,
     private _baseResponseOptions: ResponseOptions,
     private _xsrfStrategy: XSRFStrategy
   ) {}
 
-  createConnection(request: Request): CordovaHttpConnection {
+  createConnection(request: Request): MobileHttpConnection {
     this._xsrfStrategy.configureRequest(request);
-    return new CordovaHttpConnection(
+    return new MobileHttpConnection(
       request,
-      this._browserXHR,
+      this.mobileHttpClient,
       this._baseResponseOptions
     );
   }
@@ -244,10 +319,10 @@ export function _createDefaultCookieXSRFStrategy() {
 }
 
 export function httpFactory(
-  cordovaBackend: CordovaHttpBackend,
+  mobileBackend: MobileHttpBackend,
   requestOptions: RequestOptions
 ): Http {
-  return new Http(cordovaBackend, requestOptions);
+  return new Http(mobileBackend, requestOptions);
 }
 
 @NgModule({
@@ -255,12 +330,12 @@ export function httpFactory(
     {
       provide: Http,
       useFactory: httpFactory,
-      deps: [CordovaHttpBackend, RequestOptions]
+      deps: [MobileHttpBackend, RequestOptions]
     },
-    BrowserXhr,
+    MobileHttpClient,
     { provide: RequestOptions, useClass: BaseRequestOptions },
     { provide: ResponseOptions, useClass: BaseResponseOptions },
-    CordovaHttpBackend,
+    MobileHttpBackend,
     { provide: XSRFStrategy, useFactory: _createDefaultCookieXSRFStrategy }
   ]
 })
